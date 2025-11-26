06c028840ca5ffcf2205daeb1807cd0ace77bbfd
EsterOvrani <Ester.Ovrani@gmail.com>
2025-11-26 02:20:35 +0200

    remove unuse file frontend

--- frontend/src/components/shared/Header.js
+++
@@ -1,6 +0,0 @@
-// Header.js
-import React from 'react';
-
-const Header = () => <header className="header">Document Chat UI</header>;
-
-export default Header;
--- frontend/src/components/shared/Toast.js
+++
@@ -1,6 +0,0 @@
-// Toast.js
-import React from 'react';
-
-const Toast = ({ message }) => <div className="toast">{message}</div>;
-
-export default Toast;
